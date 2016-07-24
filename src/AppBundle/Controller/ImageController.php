ed3f3550ef23afd994f2a722004f74d142d328c5
Alex Wilson <a@ax.gy>
2016-07-25 00:21:46 +0100

    Adding url param and thumbnail filter.

--- src/AppBundle/Controller/ImageController.php
+++ src/AppBundle/Controller/ImageController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Liip\ImagineBundle\Exception\Binary\Loader\NotLoadableException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,20 +20,37 @@ class ImageController extends Controller
         $dataManager = $this->get('liip_imagine.data.manager');
 
         $filterConfiguration = $this->get('liip_imagine.filter.configuration');
-        $filterConfiguration->set('test', array('data_loader' => 'stream_loader'));
+        $filterConfiguration->set('test', array(
+            'data_loader' => 'stream_loader',
+            'filters' => array("thumbnail" => array(
+                    "size" => array(50, 50)
+                )
+            ),
+            'post_processors' => array(
+            )
+        ));
 
-        $binary = $dataManager->find('test', 'http://lorempixel.com/400/200/sports/1/');
+        try {
+            $url = $request->get('url', 'http://lorempixel.com/400/200/sports/1/');
+            $binary = $dataManager->find('test', $url);
+        } catch (NotLoadableException $e) {
+            throw new NotFoundHttpException("Source image could not be found", $e);
+        }
 
-        $contentLength = mb_strlen($binary->getContent());
+        $binary = $filterManager->applyFilter($binary, 'test', array());
 
+        $contentLength = mb_strlen($binary->getContent(), '8bit');
         if ($contentLength === false) {
-            throw new HttpException(503, "Unable to download source file.");
+            throw new HttpException(503, "Error occurred whilst processing image.");
         }
 
         $response = new Response($binary->getContent());
         $response->headers->set('Content-Type', $binary->getMimeType());
-        $response->headers->set('Connection', 'Keep-Alive');
-        $response->headers->set('Accept-Ranges', 'bytes');
+        $response->headers->set('Content-Length', $contentLength);
+        $response->setPublic();
+        $response->setSharedMaxAge(3600 * 72);
+        $response->setMaxAge(3600 * 72);
+        $response->setVary('Accept-Encoding');
         return $response;
     }
 }
