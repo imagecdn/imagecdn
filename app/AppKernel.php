2dff38cab9c8147b26f010c3338bbce849591f21
Alex Wilson <a@ax.gy>
2016-07-24 23:12:19 +0100

    Enabling LiipImagineBundle in AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
             new AppBundle\AppBundle(),
         ];
 
