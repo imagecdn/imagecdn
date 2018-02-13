769d156e7a55bf36039aac42bd8e6b32bc1adeb1
Alex Wilson <a@ax.gy>
2018-02-13 00:47:24 +0000

    feature(image-scaling): Implemented image scaling, with BC and quality improvements.

--- main.go
+++ main.go
@@ -27,13 +27,12 @@ func indexAction(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
 }
 
-
 func imageAction(res http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	source, _ := url.QueryUnescape(params["source"])
 
-    imagick.Initialize()
-    defer imagick.Terminate()
+	imagick.Initialize()
+	defer imagick.Terminate()
 
 	httpClient := &http.Client{
 		Timeout: time.Second * 30,
@@ -46,7 +45,7 @@ func imageAction(res http.ResponseWriter, req *http.Request) {
 	defer sourceResponse.Body.Close()
 	sourceObject, _ := ioutil.ReadAll(sourceResponse.Body)
 
-    mw := imagick.NewMagickWand()
+	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 
 	mw.ReadImageBlob(sourceObject)
@@ -69,24 +68,74 @@ func resizeImage(res http.ResponseWriter, req *http.Request, mw *imagick.MagickW
 	heights, heightOk := queryString["height"]
 	widths, widthOk := queryString["width"]
 
-	if (!heightOk && !widthOk) {
+	if !heightOk && !widthOk {
 		return
 	}
 
 	// Assume no fill, yet.
-	if (heightOk && len(heights) > 0) {
+	if heightOk && len(heights) > 0 {
 		IHeight, _ := strconv.Atoi(heights[0])
 		height = uint(IHeight)
 	} else {
 		height = mw.GetImageHeight()
 	}
-	if (widthOk && len(widths) > 0) {
+	if widthOk && len(widths) > 0 {
 		IWidth, _ := strconv.Atoi(widths[0])
 		width = uint(IWidth)
 	} else {
 		width = mw.GetImageWidth()
 	}
 
+	if height > 50000 || width > 50000 {
+		return
+	}
+
+	fits, fitOk := queryString["fit"]
+	fit := "clip"
+	if fitOk {
+		fit = fits[0]
+	}
+
+	switch fit {
+	// "clamp":
+
+	// "crop":
+
+	// "fill":
+
+	// "max":
+
+	// "min":
+
+	case "scale":
+		resizeAndScaleImage(mw, width, height)
+
+	case "clip", "contain":
+		fallthrough
+	default:
+		resizeAndClipImage(mw, width, height)
+
+	}
+}
+
+func resizeAndClipImage(mw *imagick.MagickWand, width uint, height uint) {
+	var widthRatio, heightRatio, ratio float64
+
+	widthRatio = float64(width) / float64(mw.GetImageWidth())
+	heightRatio = float64(height) / float64(mw.GetImageHeight())
+
+	ratio = heightRatio
+	if widthRatio < heightRatio {
+		ratio = widthRatio
+	}
+
+	width = uint(float64(mw.GetImageWidth()) * ratio)
+	height = uint(float64(mw.GetImageHeight()) * ratio)
+
+	resizeAndScaleImage(mw, width, height)
+}
+
+func resizeAndScaleImage(mw *imagick.MagickWand, width uint, height uint) {
 	mw.ResizeImage(width, height, imagick.FILTER_LANCZOS)
 }
 
@@ -94,18 +143,18 @@ func formatImage(res http.ResponseWriter, req *http.Request, mw *imagick.MagickW
 	queryString := req.URL.Query()
 	imageFormats, ok := queryString["format"]
 
-	if (!ok || len(imageFormats) == 0) {
+	if !ok || len(imageFormats) == 0 {
 		return
 	}
 
 	imageFormat := imageFormats[0]
 
 	var imageFormatMap = map[string]string{
-		"jpg": "image/jpg",
-		"png": "image/png",
+		"jpg":  "image/jpg",
+		"png":  "image/png",
 		"webp": "image/webp",
-		"svg": "image/svg",
-		"gif": "image/gif",
+		"svg":  "image/svg",
+		"gif":  "image/gif",
 	}
 
 	mw.SetFormat(imageFormat)
