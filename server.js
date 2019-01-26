59e43f654097ad912afb934f28aa70ebbee39098
Alex Wilson <a@ax.gy>
2019-01-26 03:21:45 +0000

    proxy to docs and cache downloaded images

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http')
 const url = require('url')
-const querystring = require('querystring')
+const os = require('os')
 const connect = require('connect')
-const fetch = require('node-fetch')
+const httpProxy = require('http-proxy')
+const fetch = require('make-fetch-happen').defaults({
+    cacheManager: os.tmpdir()
+})
 const Parameters = require('./lib/parameters')
 const mime = require('mime-types')
 const fileType = require('file-type')
@@ -18,14 +21,22 @@ const port = process.env.PORT || 3000
 app.use(acceptReader)
 app.use(urlReader)
 
-app.use('/v1/images/', function(req, res) {
+app.use('/v2/health', function(req, res) {
+    return res.end(JSON.stringify({
+        status: 'OK',
+        latestCheck: Date.now()
+    }))
+})
+app.use('/v2/image/', function(req, res) {
     const reqUrl = url.parse(req.url)
 
     const parameters = new Parameters(Object.assign({
         uri: decodeURIComponent(reqUrl.pathname.replace(/^\//, ''))
     }, req.query))
 
-    return fetch(parameters.uri)
+    return fetch(parameters.uri, {
+        cache: 'force-cache'
+    })
         .then(res => res.buffer())
         .then(buffer => {
             if (!parameters.format) {
@@ -49,11 +60,34 @@ app.use('/v1/images/', function(req, res) {
         .then(buffer => transformBuffer(parameters)(buffer))
         .then(buffer => compressBuffer(parameters)(buffer))
         .then(image => {
-            console.log(image)
             res.setHeader('Content-Length', image.byteLength)
             res.setHeader('Content-Type', mime.contentType(parameters.format))
             return res.end(image)
         })
 })
+// Handle redirects from /v1/ service to /v2/
+app.use('/v1/images/', (req, res) => {
+    res.setHeader('Location', req.originalUrl.replace('/v1/images/', '/v2/image/'))
+    res.statusCode = 301
+    return res.end()
+})
 
-http.createServer(app).listen(port)
+// Fall-back to docsite for unknown routes.
+const proxyToOrigin = (req, res) => {
+    req.url = req.originalUrl
+    return httpProxy.createProxyServer({
+        target: 'http://imagecdn.github.io',
+        headers: {
+            'Host': 'imagecdn.app'
+        }
+    }).web(req, res);
+}
+app.use('/getting-started', proxyToOrigin)
+app.use('/docs', proxyToOrigin)
+app.use('/about', proxyToOrigin)
+app.use('/', proxyToOrigin)
+app.use('/*', proxyToOrigin)
+
+const server = http.createServer(app).listen(port, _ => {
+    console.log(`Now listening on ${server.address().port}`)
+})
