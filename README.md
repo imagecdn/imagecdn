eef9667065ebdc7a91f270cd4664d69664c82c5c
Alex Wilson <alex@alexwilson.tech>
2024-02-14 00:23:32 +0000

    feat: Major upgrade to Node 18

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A free image resizing service and CDN.
 
-### Why?
+## Why?
 This started out out of a repeated frustration that optimising images between multiple
 projects was a constantly moving target, requiring large amounts of boilerplate. Image
 libraries existed but went about solving the same problems in different ways to varying
@@ -14,10 +14,20 @@ either via Docker (AWS ECS/Kubernetes) or directly via Node.
 
 The public instance of this service is available here: https://imagecdn.app/
 
-### Rough Architecture
+## Rough Architecture
 - HTTP Request comes in, specifying image source.
 - Image Source is streamed.
 - Format determined by QueryString parameter, falling back to Accept header.
 - Image is streamed to libvips orchestration layer for conversion and manipulation.
 - Image is run through image optimisation pipeline.
 - Image served with a Vary on Accept and Accept-Encoding.
+
+## Getting Started
+
+### macOS
+
+$ brew install libimagequant
+
+### Ubuntu
+
+$ sudo apt-get install libimagequant-dev
--- server.js
+++ server.js
@@ -1,142 +1,173 @@
-const http = require('http')
-const url = require('url')
-const os = require('os')
-const connect = require('connect')
-const httpProxy = require('http-proxy')
-const throng = require('throng')
-const fetch = require('make-fetch-happen').defaults({
-    cacheManager: os.tmpdir()
-})
+import http from "http";
+import url from "url";
+import os from "os";
+import connect from "connect";
+import httpProxy from "http-proxy";
+import throng from "throng";
+import makeFetchHappen from "make-fetch-happen";
 
-const Parameters = require('./lib/parameters')
-const mime = require('mime-types')
-const fileType = require('file-type')
+import Parameters from "./lib/parameters.js";
+import mime from "mime-types";
+import { fileTypeFromBuffer } from "file-type";
 
-const acceptReader = require('./lib/connect/acceptReader')
-const urlReader = require('./lib/connect/urlReader')
-const transformBuffer = require('./lib/transform/transformBuffer')
-const compressBuffer = require('./lib/compress/compressBuffer')
+import acceptReader from "./lib/connect/acceptReader.js";
+import urlReader from "./lib/connect/urlReader.js";
+import transformBuffer from "./lib/transform/transformBuffer.js";
+import compressBuffer from "./lib/compress/compressBuffer.js";
 
-const app = connect()
-const port = process.env.PORT || 3000
-const workers = process.env.WEB_CONCURRENCY || 1
+const app = connect();
+const port = process.env.PORT || 3000;
+const workers = process.env.WEB_CONCURRENCY || 1;
 
-app.use(acceptReader)
-app.use(urlReader)
+const fetch = makeFetchHappen.defaults({
+  cacheManager: os.tmpdir(),
+});
 
-app.use('/v2/health', function(req, res) {
-    res.setHeader('Cache-Control', [
-        'private',
-        'max-age=0',
-        'no-cache',
-        'no-store',
-        'must-revalidate'
-    ].join(', '))
-    res.setHeader('Expires', new Date(Date.now() - 1000).toUTCString())
-    return res.end(JSON.stringify({
-        status: 'OK',
-        latestCheck: Date.now()
-    }))
-})
-app.use('/v2/image/', function(req, res) {
-    const reqUrl = url.parse(req.url)
+// import fetch = require('make-fetch-happen').defaults({
+//   cacheManager: os.tmpdir()
+// })
 
-    const parameters = new Parameters(Object.assign({
-        uri: decodeURIComponent(reqUrl.pathname.replace(/^\//, ''))
-    }, req.query))
+app.use(acceptReader);
+app.use(urlReader);
 
-    return fetch(parameters.uri, {
-        cache: 'force-cache'
+app.use("/v2/health", function (req, res) {
+  res.setHeader(
+    "Cache-Control",
+    ["private", "max-age=0", "no-cache", "no-store", "must-revalidate"].join(
+      ", ",
+    ),
+  );
+  res.setHeader("Expires", new Date(Date.now() - 1000).toUTCString());
+  return res.end(
+    JSON.stringify({
+      status: "OK",
+      latestCheck: Date.now(),
+    }),
+  );
+});
+app.use("/v2/image/", function (req, res) {
+  const reqUrl = url.parse(req.url);
+
+  const parameters = new Parameters(
+    Object.assign(
+      {
+        uri: decodeURIComponent(reqUrl.pathname.replace(/^\//, "")),
+      },
+      req.query,
+    ),
+  );
+
+  return (
+    fetch(parameters.uri, {
+      cache: "force-cache",
     })
-        .catch(err => {
-            console.log(err)
-            res.statusCode = 404
-            return res.end(JSON.stringify({
-                error: "Image not found."
-            }))
-        })
+      .catch((err) => {
+        console.log(err);
+        res.statusCode = 404;
+        return res.end(
+          JSON.stringify({
+            error: "Image not found.",
+          }),
+        );
+      })
 
-        .then(res => res.buffer())
-        .then(buffer => {
-            if (!parameters.format) {
-                const {ext} = fileType(buffer)
+      .then((res) => res.buffer())
+      .then(async (buffer) => {
+        if (!parameters.format) {
+          const { ext } = await fileTypeFromBuffer(buffer);
 
-                // We treat WebP and JPG as one and the same.
-                // This allows older browsers to be served the right image format.
-                if (ext === 'jpg' || ext === 'webp') {
-                    parameters.format = 'jpg'
-                    if (req.alternativeFormats.has('jpg')) {
-                        parameters.format = req.alternativeFormats.get('jpg')
-                    }
-
-                //
-                } else if (ext === 'png') {
-                    parameters.format = 'png'
-                }
+          // We treat WebP and JPG as one and the same.
+          // This allows older browsers to be served the right image format.
+          if (ext === "jpg" || ext === "webp") {
+            parameters.format = "jpg";
+            if (req.alternativeFormats.has("jpg")) {
+              parameters.format = req.alternativeFormats.get("jpg");
             }
-            return buffer
-        })
-        .then(buffer => transformBuffer(parameters)(buffer))
-        .then(buffer => compressBuffer(parameters)(buffer))
-        .then(image => {
-            res.setHeader('Content-Length', image.byteLength)
-            res.setHeader('Content-Type', mime.contentType(parameters.format))
-            res.setHeader('ICDN-Format', parameters.format)
 
-            // Instruct upstream proxies to cache this for a month.
-            const cacheTtl = 60 * 60 * 24 * 30
-            res.setHeader('Cache-Control', `public, max-age=${cacheTtl} s-maxage=${cacheTtl}`)
-            res.setHeader('Expires', new Date(Date.now() + cacheTtl*1000).toUTCString())
+            //
+          } else if (ext === "png") {
+            parameters.format = "png";
+          }
+        }
+        return buffer;
+      })
+      .then((buffer) => transformBuffer(parameters)(buffer))
+      .then((buffer) => compressBuffer(parameters)(buffer))
+      .then((image) => {
+        res.setHeader("Content-Length", image.byteLength);
+        res.setHeader("Content-Type", mime.contentType(parameters.format));
+        res.setHeader("ICDN-Format", parameters.format);
 
-            // Allow CORS from everywhere for more advanced image use-cases.
-            res.setHeader('Access-Control-Allow-Origin', '*')
+        // Instruct upstream proxies to cache this for a month.
+        const cacheTtl = 60 * 60 * 24 * 30;
+        res.setHeader(
+          "Cache-Control",
+          `public, max-age=${cacheTtl} s-maxage=${cacheTtl}`,
+        );
+        res.setHeader(
+          "Expires",
+          new Date(Date.now() + cacheTtl * 1000).toUTCString(),
+        );
 
-            return res.end(image)
-        })
+        // Allow CORS from everywhere for more advanced image use-cases.
+        res.setHeader("Access-Control-Allow-Origin", "*");
 
-        // Generic error handling.
-        .catch(err => {
-            console.error(err)
-            res.statusCode = 503
-            return res.end(JSON.stringify({
-                error: "An unexpected error occurred, if the issue persists please get in touch with imagecdn.support@imagecdn.app"
-            }))
-        })
-})
+        return res.end(image);
+      })
+
+      // Generic error handling.
+      .catch((err) => {
+        console.error(err);
+        res.statusCode = 503;
+        return res.end(
+          JSON.stringify({
+            error:
+              "An unexpected error occurred, if the issue persists please get in touch with imagecdn.support@imagecdn.app",
+          }),
+        );
+      })
+  );
+});
 // Handle redirects from /v1/ service to /v2/
-app.use('/v1/images/', (req, res) => {
-    res.setHeader('Location', req.originalUrl
-        // Endpoint is now singular.
-        .replace('/v1/images/', '/v2/image/')
-        // Fill is now Fit to reflect common terminology.
-        .replace('fill=', 'fit=')
-    )
-    res.statusCode = 301
-    return res.end()
-})
+app.use("/v1/images/", (req, res) => {
+  res.setHeader(
+    "Location",
+    req.originalUrl
+      // Endpoint is now singular.
+      .replace("/v1/images/", "/v2/image/")
+      // Fill is now Fit to reflect common terminology.
+      .replace("fill=", "fit="),
+  );
+  res.statusCode = 301;
+  return res.end();
+});
 
 // Fall-back to docsite for unknown routes.
 const proxyToOrigin = (req, res) => {
-    req.url = req.originalUrl
-    return httpProxy.createProxyServer({
-        target: 'http://imagecdn.github.io',
-        headers: {
-            'Host': 'imagecdn.app'
-        }
-    }).web(req, res);
-}
-app.use('/getting-started', proxyToOrigin)
-app.use('/docs', proxyToOrigin)
-app.use('/about', proxyToOrigin)
-app.use('/', proxyToOrigin)
-app.use('/*', proxyToOrigin)
+  req.url = req.originalUrl;
+  return httpProxy
+    .createProxyServer({
+      target: "http://imagecdn.github.io",
+      headers: {
+        Host: "imagecdn.app",
+      },
+    })
+    .web(req, res);
+};
+app.use("/getting-started", proxyToOrigin);
+app.use("/docs", proxyToOrigin);
+app.use("/about", proxyToOrigin);
+app.use("/", proxyToOrigin);
+app.use("/*", proxyToOrigin);
 
 async function main() {
-    const server = http.createServer(app).listen(port, _ => {
-        console.log(`Now listening on ${server.address().port}`)
-    })
+  const server = http.createServer(app).listen(port, (_) => {
+    console.log(`Now listening on ${server.address().port}`);
+  });
 }
+
 throng({
-    workers,
-    lifetime: Infinity
-}, main)
+  workers,
+  lifetime: Infinity,
+  worker: main,
+});
